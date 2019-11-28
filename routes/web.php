52847109cd78d15e371c0bdeab2847a7fb49d42f
ilhamsj <ilhamsaputrajati@gmail.com>
2019-11-28 10:41:46 +0700

    blog

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/berita', function () {
+    return view('show');
+});
+
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
