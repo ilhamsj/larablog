5ef98f1216d34cf0570a5c8a4a8d9f7adbeea76b
ilhamsj <ilhamsaputrajati@gmail.com>
2019-11-28 11:20:15 +0700

    env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,47 @@
-<h1>Helo</h1>
-{{-- {{ env('app_name') }} --}}
-{{ env('APP_NAME') }}
+@extends('layouts.user')
+
+@section('content')
+      <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Clean Blog</h1>
+            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        @for ($i = 0; $i < 10; $i++)
+          <div class="post-preview">
+            <a href="/berita">
+              <h2 class="post-title">
+                {{-- Man must explore, and this is exploration at its greatest --}}
+              </h2>
+              <h3 class="post-subtitle">
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">
+              </a>
+              on September 24, 2019
+            </p>
+          </div>
+          <hr>
+        @endfor
+        <!-- Pager -->
+        <div class="clearfix">
+          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
