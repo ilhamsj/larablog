1815a54dd870b5f0fadf4181338336caa9f29601
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-12 12:01:16 +0700

    kritik dan saran

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -17,7 +17,7 @@
     .whatsapp {
       transform: rotate(-90deg);
       bottom: 50vh;
-      right: -45px;
+      right: -55px;
     }
   </style>
   @stack('styles')
@@ -121,15 +121,56 @@
         @endforeach
       </div>
     </div>
-  </footer>
-  <section class="whatsapp position-fixed rounded-left">
-      <a href="" class="btn btn-primary rounded-top">Whatsapp</a>
+  </footer>  
+  <section class="whatsapp position-fixed rounded-left" id="saran">
+      <a href="" class="btn btn-primary rounded-top">Kritik Saran</a>
   </section>
+
+  <!-- Modal -->
+  <div class="modal fade" id="kritik_saran" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+        <form action="">
+          @csrf
+          <div class="form-group">
+            <label for="">Nama</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+          </div>
+
+          <div class="form-group">
+            <label for="">Email</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+          </div>
+
+          <div class="form-group">
+            <label for="">Kritik dan Saran</label>
+            <textarea class="form-control" name="" id="" rows="3"></textarea>
+          </div>
+        </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <script src="{{ secure_url('js/app.js') }}"></script>
   <script>
     $('#{{ Str::slug(env("APP_NAME")) }}').toggleClass('col col-6');
     $('#copyright').toggleClass('col col-12 mt-4').find('h4').remove();
-
+    $('#saran').click(function (e) { 
+      e.preventDefault();
+      $('#kritik_saran').modal('show')
+    });
   </script>
   @stack('scripts')
 </body>
