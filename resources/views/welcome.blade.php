e97484e07cf976766d370611a952514c5ebdd4b4
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-20 18:31:38 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,27 @@
 @section('title_page', 'Welcome')
 
 @section('header')
-  <header class="masthead">
-    <div class="overlay"></div>
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        @foreach ($slider as $item)
-        @if (env('APP_ENV') == 'local')
-        <div class="swiper-slide text-center" style="max-height:100vh">
-          <img class="img-fluid" data-src="holder.js/1366x768?auto=yes&random=yes&textmode=exact" alt="" srcset="">
-        </div>
-        @else
-        <div class="swiper-slide text-center" style="max-height:100vh">
-          <img class="img-fluid" src="{{$item->file}}" alt="" srcset="">
-        </div>
-        @endif
-        @endforeach
+<header class="masthead">
+  <div class="overlay"></div>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      @foreach ($slider as $item)
+      @if (env('APP_ENV') == 'local')
+      <div class="swiper-slide text-center" style="max-height:100vh">
+        <img class="img-fluid" data-src="holder.js/1366x768?auto=yes&random=yes&textmode=exact" alt="" srcset="">
       </div>
-      <div class="swiper-pagination"></div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
+      @else
+      <div class="swiper-slide text-center" style="max-height:100vh">
+        <img class="img-fluid" src="{{$item->file}}" alt="" srcset="">
+      </div>
+      @endif
+      @endforeach
     </div>
-  </header>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+</header>
 @endsection
 
 @section('content')
@@ -38,22 +38,22 @@
     <span style="font-size: medium">
       <i class="fas fa-calendar-alt"></i>
       {{ $articles->last()->created_at->format('d F Y') }}
-      
+
       <i class="fa fa-comments ml-4" aria-hidden="true"></i>
-      {{ count($articles->last()->Review)}} 
+      {{ count($articles->last()->Review)}}
       Komentar
 
       <i class="fa fa-tag ml-4"></i>
-      <a href="" class=""> {{ $articles->last()->category}}</a> 
+      <a href="" class=""> {{ $articles->last()->category}}</a>
     </span>
     <p>
       @if (env('APP_ENV') == 'local')
-        <img class="img-fluid rounded" data-src="holder.js/1366x768?auto=yes&random=yes&textmode=exact" alt="" srcset="">    
+      <img class="img-fluid rounded" data-src="holder.js/1366x768?auto=yes&random=yes&textmode=exact" alt="" srcset="">
       @else
-        <img class="img-fluid rounded" src="{{ $articles->last()->cover }}" alt="" srcset="">
+      <img class="img-fluid rounded" src="{{ $articles->last()->cover }}" alt="" srcset="">
       @endif
     </p>
-    {!! strip_tags(Str::limit($articles->last()->content, 100, '')) !!} 
+    {!! strip_tags(Str::limit($articles->last()->content, 100, '')) !!}
     <a href="{{ route('user.artikel.show', $articles->last()->slug) }}">
       <strong>Pelajari selengkapnya <i class="fa fa-arrow-circle-right" aria-hidden="true"></i> </strong>
     </a>
@@ -65,32 +65,32 @@
     <hr>
   </div>
   @foreach ($articles as $item)
-    <div class="col-12 col-sm-6 mb-4">
-      @if (env('APP_ENV') == 'local')
-        <img class="img-fluid rounded" data-src="holder.js/800x600?auto=yes&random=yes&textmode=exact" alt="" srcset="">
-      @else
-        <img class="img-fluid rounded" src="{{ $item->cover }}" alt="" srcset="">
-      @endif
-    </div>
-    <div class="col-12 col-sm-6 mb-4">
-      <p>
-        <h3>
-          <a href="{{ route('user.artikel.show', $item->slug) }}">{{ $item->title }}</a>
-        </h3>
-        <span style="font-size: medium">
-          <i class="fas fa-calendar-alt"></i>
-          {{ $item->created_at->format('d M Y') }}
-          
-          <i class="fa fa-comments ml-2" aria-hidden="true"></i>
-          {{ count($item->Review)}} 
-          Komentar
+  <div class="col-12 col-sm-6 mb-4">
+    @if (env('APP_ENV') == 'local')
+    <img class="img-fluid rounded" data-src="holder.js/800x600?auto=yes&random=yes&textmode=exact" alt="" srcset="">
+    @else
+    <img class="img-fluid rounded" src="{{ $item->cover }}" alt="" srcset="">
+    @endif
+  </div>
+  <div class="col-12 col-sm-6 mb-4">
+    <p>
+      <h3>
+        <a href="{{ route('user.artikel.show', $item->slug) }}">{{ $item->title }}</a>
+      </h3>
+      <span style="font-size: medium">
+        <i class="fas fa-calendar-alt"></i>
+        {{ $item->created_at->format('d M Y') }}
 
-          <i class="fa fa-tag ml-2"></i>
-          <a href="" class=""> {{ $item->category}}</a> 
-        </span>
-      </p>
-      {!! strip_tags(Str::limit($item->content, 100)) !!} 
-    </div>
+        <i class="fa fa-comments ml-2" aria-hidden="true"></i>
+        {{ count($item->Review)}}
+        Komentar
+
+        <i class="fa fa-tag ml-2"></i>
+        <a href="" class=""> {{ $item->category}}</a>
+      </span>
+    </p>
+    {!! strip_tags(Str::limit($item->content, 100)) !!}
+  </div>
   <div class="w-100"></div>
   @endforeach
 </div>
@@ -102,20 +102,20 @@
     <div class="row">
       <div class="col">
         <div class="row parent-container">
-            <div class="col-12">
-              <strong class="">Galeri <span class="text-primary">Kegiatan</span></strong>
-              <hr>
-            </div>
-            @foreach ($photos as $item)
-            <div class="col-6 col-sm-3 mb-4">
-              @if (env('APP_ENV') == 'local')
-                <img style="cursor: pointer" class="img-fluid rounded"  href="holder.js/1366x768?auto=yes&random=yes&textmode=exact" data-src="holder.js/800x600?auto=yes&random=yes&textmode=exact" alt="" srcset="">
-              @else
-                <img style="cursor: pointer" class="img-fluid rounded"  href="{{ secure_url($item->file) }}" src="{{ secure_url($item->file) }}" alt="" srcset="">
-              @endif
-        
-            </div>
-            @endforeach
+          <div class="col-12">
+            <strong class="">Galeri <span class="text-primary">Kegiatan</span></strong>
+            <hr>
+          </div>
+          @foreach ($photos as $item)
+          <div class="col-6 col-sm-3 mb-4">
+            @if (env('APP_ENV') == 'local')
+            <img style="cursor: pointer" class="img-fluid rounded" href="holder.js/1366x768?auto=yes&random=yes&textmode=exact" data-src="holder.js/800x600?auto=yes&random=yes&textmode=exact" alt="" srcset="">
+            @else
+            <img style="cursor: pointer" class="img-fluid rounded" href="{{ secure_url($item->file) }}" src="{{ secure_url($item->file) }}" alt="" srcset="">
+            @endif
+
+          </div>
+          @endforeach
         </div>
       </div>
     </div>
@@ -126,22 +126,24 @@
 @push('styles')
 <link rel="stylesheet" href="{{ secure_url('vendor/swiper.min.css') }}">
 <style>
-  body {background-color: white}
+  body {
+    background-color: white
+  }
 </style>
 @endpush
 
 @push('scripts')
-  <script src="{{ secure_url('vendor/swiper.min.js') }}"></script>
-  <script src="{{ secure_url('vendor/holder.js') }}"></script>
-  <script>
-  $(document).ready(function () {
-    
+<script src="{{ secure_url('vendor/swiper.min.js') }}"></script>
+<script src="{{ secure_url('vendor/holder.js') }}"></script>
+<script>
+  $(document).ready(function() {
+
     // gallery
     $('.parent-container').magnificPopup({
       delegate: 'img',
       type: 'image',
-      gallery:{
-        enabled:true
+      gallery: {
+        enabled: true
       }
     });
 
@@ -163,5 +165,5 @@
       },
     });
   });
-  </script>
+</script>
 @endpush
