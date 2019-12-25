519ca87201f8356411b027eda0e8837a5f801064
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-25 19:38:19 +0700

    faker

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        'user_id'   => \App\User::find(rand(1, 2)),
+        'user_id'   => 1,
         'category'  => $faker->randomElement(['Review', 'Komentar']),
         'content'   => $faker->realText($maxNbChars = 200, $indexSize = 5),
     ];
